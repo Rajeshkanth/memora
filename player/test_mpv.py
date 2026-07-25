d78f3d2e8b9586ea6f32d5df6cca9cf51375e8e0
Rajeshkanth <rajeshkanth@solinas.in>
2026-07-25 21:35:57 +0530

    test

--- player/test_mpv.py
+++ player/test_mpv.py
@@ -1,16 +1,30 @@
 import time
 import mpv
 
+finished = False
+
+
+def on_end(event):
+    global finished
+    print("Playback finished")
+    finished = True
+
+
 player = mpv.MPV(
     fullscreen=True,
     keep_open=False,
 )
 
+player.register_event_callback(on_end)
+
+print("Starting video...")
+
 player.play("media/test.mp4")
 
-print("Playing...")
+# Give MPV time to start playback
+time.sleep(1)
 
-while player.core_idle is False:
+while not finished:
     time.sleep(0.1)
 
-print("Finished")
+print("Done")
