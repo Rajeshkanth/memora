f32b78365b3164ed21a9960251e2548b90b14afe
Rajeshkanth <rajeshkanth@solinas.in>
2026-07-25 17:56:05 +0530

    test

--- player/utils.py
+++ player/utils.py
@@ -17,8 +17,8 @@ VIDEO_EXTENSIONS = {
     ".webm"
 }
 
-def _is_image(path):
+def is_image(path):
     return Path(path).suffix.lower() in IMAGE_EXTENSIONS
 
-def _is_video(path):
+def is_video(path):
     return Path(path).suffix.lower() in VIDEO_EXTENSIONS
