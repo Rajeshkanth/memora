0670d27b061b18ea0e0999a922469c0109291dcb
Rajeshkanth <rajeshkanth@solinas.in>
2026-07-29 18:24:15 +0530

    term

--- player/engines/video_engine.py
+++ player/engines/video_engine.py
@@ -24,7 +24,7 @@ class VideoEngine:
                 "mpv",
                 "--fullscreen",
                 "--vo=gpu",
-                "--no-terminal"
+                "--no-terminal",
                 video_path,
             ],
             stdin=subprocess.DEVNULL,
